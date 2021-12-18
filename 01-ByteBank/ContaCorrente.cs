7639e98f34733b410ce02459205c4de519794508
Greice Felipe <greice.n.felipe@gmail.com>
2021-12-18 19:50:11 -0300

    Meu primeiro código em C#.:tada:

---
+++ 01-ByteBank/ContaCorrente.cs
@@ -0,0 +1,79 @@
+﻿using System;
+namespace ByteBank
+{
+    public class ContaCorrente
+    {
+        public Cliente Titular { get; set; }
+        public int Numero { get; set; }
+       
+        public static int TotalDeContasCriadas { get; private set; }
+
+
+        public ContaCorrente(int agencia, int numero)
+        {
+            Agencia = agencia;
+            Numero = numero;
+            TotalDeContasCriadas++;
+        }
+
+        private int _agencia;
+        public int Agencia
+        {
+            get
+            {
+                return _agencia;
+            }
+            set
+            {
+                if(value >= 0)
+                {
+                    _agencia = value;
+                }
+            }
+        }
+
+        private double _saldo;
+        public Double Saldo
+        {
+            get
+            {
+                return _saldo;
+            }
+            set
+            {
+                if(value > 0)
+                {
+                    _saldo = value;
+                }
+            }
+        }
+
+        public void Depositar(double valor)
+        {
+            this._saldo += valor;
+        }
+
+        public bool Sacar(double valor)
+        {
+            if(this._saldo < valor)
+            {
+                return false;
+            }
+
+            this._saldo -= valor;
+            return true;
+        }
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            bool saque = this.Sacar(valor);
+
+            if (saque)
+            {
+                contaDestino.Depositar(valor);
+            }
+
+            return saque;
+        }
+    }
+}
---
+++ 01-ByteBank/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executanto ByteBank");
+
+            Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
+
+            ContaCorrente conta = new ContaCorrente(876, 86712540);
+            Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
+
+            Console.WriteLine(conta.Agencia);
+            Console.WriteLine(conta.Numero);
+
+            ContaCorrente contaDaGabriela = new ContaCorrente(867, 86745820);
+            Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
+
+            conta.Depositar(200);
+            Console.WriteLine(conta.Saldo);
+
+            Console.ReadLine();
+        }
+    }
+}
